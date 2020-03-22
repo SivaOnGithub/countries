04c37d9cbb8da1651ebf323f51b23be36778fb37
Siva <44206012+r-siva@users.noreply.github.com>
2020-03-22 19:09:23 +0530

    Initial commit

---
+++ precache-manifest.b142de09d926e288baa8d08e23ceb9bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50ae158aaa19edb8814aea8fbb7b024",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07c7aff3df3cdfcf3395",
+    "url": "/static/css/main.f60780ec.chunk.css"
+  },
+  {
+    "revision": "6887bdeb2e33edaa4594",
+    "url": "/static/js/2.97cbc80e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.97cbc80e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c7aff3df3cdfcf3395",
+    "url": "/static/js/main.b8cbb816.chunk.js"
+  },
+  {
+    "revision": "bfd555ac1caf6df0cfb4",
+    "url": "/static/js/runtime-main.68f91701.js"
+  }
+]);
